075b73f19252061d52262b5102da0a7f8c989d91
elcimarsilva <elcimar8@gmail.com>
2024-04-28 22:27:42 -0300

    add some tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,4 +5,8 @@ class CalcTests(SimpleTestCase):
     def test_add_number(self):
         """ test calc module"""
         response = calc.add(5,6)
-        self.assertEqual(response, 11)        
+        self.assertEqual(response, 11)
+
+    def test_subtract(self):
+        response = calc.subtract(10, 15)
+        self.assertEqual(response, 5)
