8871883493a28586646c730f4870afc41e6feadf
elcimarsilva <elcimar8@gmail.com>
2024-09-15 00:49:37 -0300

    enable host for ubuntu

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #bool(int(os.environ.get('DEBUG', 0))) # fix CSS bug
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', '0.0.0.0']
 ALLOWED_HOSTS.extend(
     filter(
         None,
